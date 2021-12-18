ef08229dce4056f026f688b1b650be1577e63a86
William Angelis <willangelis@gmail.com>
2021-12-17 23:04:59 -0300

    Transformando funções normais em arrow functions

--- main.js
+++ main.js
@@ -4,24 +4,18 @@ const dimNum = document.getElementById("btn_dimNum");
 const zerarNum = document.getElementById("btn_zerarNum");
 const dobrar = document.getElementById("btn_dobrar");
 const triplicar = document.getElementById("btn_triplicar")
-const contador = document.getElementById("btn_contador");
+const contador = document.querySelector(".contador");
 
 let contagem = 0;
-contador.innerHTML = contagem;
 
-aumNum.addEventListener("click", function aumentar(){
-    contador.innerHTML = ++contagem
-});
+contador.textContent = contagem;
 
-dimNum.addEventListener("click", function diminuir(){
-    contador.innerHTML = contagem =  --contagem
-});
-zerarNum.addEventListener("click",function zerar(){
-    contador.innerHTML = contagem = 0;
-});
-dobrar.addEventListener("click", function dobrar(){
-    contador.innerHTML = contagem = contagem * 2;
-});
-triplicar.addEventListener("click", function triplicar(){
-    contador.innerHTML = contagem = contagem * 3;
-});
+aumNum.addEventListener("click",() => contador.innerHTML = ++contagem ); //bnt aumentar 
+
+dimNum.addEventListener("click",() => contador.innerHTML = contagem =  --contagem); // btn diminuir
+
+zerarNum.addEventListener("click",() => contador.innerHTML = contagem = 0); // btn zerar
+
+dobrar.addEventListener("click", () => contador.innerHTML = contagem = contagem * 2); // btn dobrar
+
+triplicar.addEventListener("click", () => contador.innerHTML = contagem = contagem * 3); // btn triplicar
